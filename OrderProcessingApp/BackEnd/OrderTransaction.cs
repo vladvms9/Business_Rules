c482e16e5473d13f77d593d163502101852c5500
vladvms9 <88330630+vladvms9@users.noreply.github.com>
2021-08-02 23:09:14 +0800

    Created TestMethods: AddToCartTest_Success01 AddToCartTest_Sucsess02 AddToCartTest_Fail01 CheckOutTest_Success01 CheckOutTest_Fail01

--- OrderProcessingApp/BackEnd/OrderTransaction.cs
+++ OrderProcessingApp/BackEnd/OrderTransaction.cs
@@ -8,13 +8,24 @@ namespace BackEnd
 {
     public class OrderTransaction
     {
+        private Dictionary<int, Order> orders;
+
+
         public OrderTransaction()
         { 
 
         }
 
-        public void AddToCart()
+        /// <summary>
+        /// AddToCart
+        /// To simulate multiple orders with same product, a quantity param is introduced.
+        /// </summary>
+        /// <param name="prod">Product</param>
+        /// <param name="quantity">Quantity</param>
+        public void AddToCart(Product prod, int quantity)
         {
+            if (prod == null || quantity <= 0)
+                throw new ApplicationException("Invalid product or quantity.");
 
         }
 
--- OrderProcessingApp/OrderProcessing.Tests/OrderTransactionTests.cs
+++ OrderProcessingApp/OrderProcessing.Tests/OrderTransactionTests.cs
@@ -12,7 +12,37 @@ namespace BackEnd.Tests
     public class OrderTransactionTests
     {
         [TestMethod()]
-        public void AddToCartTest()
+        // Single Order
+        public void AddToCartTest_Success01()
+        {
+            OrderTransaction trans = new OrderTransaction();
+            trans.AddToCart(null, 0);
+        }
+
+        [TestMethod()]
+        // Multiple Orders
+        public void AddToCartTest_Success02()
+        {
+            OrderTransaction trans = new OrderTransaction();
+            Product p = new Product();
+            trans.AddToCart(p, 0);
+        }
+
+        [TestMethod()]
+        // No orders specified
+        public void AddToCartTest_Fail01()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CheckOutTest_Success01()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CheckOutTest_Fail01()
         {
             Assert.Fail();
         }

eae0e107d6167dd36b73b6c27846331bd7dae94b
vladvms9 <88330630+vladvms9@users.noreply.github.com>
2021-08-02 22:44:53 +0800

    Added initials Unit Test for AddToCart Method

---
+++ OrderProcessingApp/OrderProcessing.Tests/OrderTransactionTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BackEnd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.Tests
+{
+    [TestClass()]
+    public class OrderTransactionTests
+    {
+        [TestMethod()]
+        public void AddToCartTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

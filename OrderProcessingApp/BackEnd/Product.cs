d8339664dc97b68d22c6ef3d5d6e9778cb3f9cec
vladvms9 <88330630+vladvms9@users.noreply.github.com>
2021-08-02 22:25:31 +0800

    Created BackEnd Project Added classes Action and Product

---
+++ OrderProcessingApp/BackEnd/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Action> Action { get; set; }
+
+        public Product()
+        {
+            this.Action = new List<Action>();
+        }
+    }
+}

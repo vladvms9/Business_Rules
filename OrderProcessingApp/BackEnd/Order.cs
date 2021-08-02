0519d7e3dde01c7270cc14de290d9ca7bafaf9f6
vladvms9 <88330630+vladvms9@users.noreply.github.com>
2021-08-02 22:33:21 +0800

    Added class Order

---
+++ OrderProcessingApp/BackEnd/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd
+{
+    public class Order
+    {
+        public Product Product { get; set; }
+    }
+}
